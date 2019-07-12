2f829fd64643216ef7d0946bc2799005490aa003
wangzezhou <wangzezhou2561@dingtalk.com>
2019-07-12 16:42:39 +0800

    未注明人士的提交

---
+++ src/main/java/com/pea2u/letter2future/config/InterceptorConfig.java
@@ -0,0 +1,16 @@
+package com.pea2u.letter2future.config;
+
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author wangzz
+ * @date 2019年7月11日19:39:09
+ * 拦截器配置类
+ */
+public class InterceptorConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+}
